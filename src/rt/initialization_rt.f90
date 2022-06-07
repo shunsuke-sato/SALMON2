095ce85c91747afc1ad95e8b82bc923873ee4226
Shunsuke A. Sato <shunsuke.sato26@gmail.com>
2022-06-07 16:46:56 +0900

    Fixed a bug.

--- src/rt/initialization_rt.f90
+++ src/rt/initialization_rt.f90
@@ -498,17 +498,20 @@ subroutine initialization_rt( Mit, system, energy, ewald, rt, md, &
        call write_dns_ac_je(info,mg,system,rho%f,singlescale,itt,"new")
        call write_dns_ac_je(info,mg,system,rho%f,singlescale,itt,"bin")
     end if
-
-    if(yn_transition_current_density=='y')then
-      allocate(rt%zj_e_tcd(nmax_omega_tcd))
-      do itmp = 1, nmax_omega_tcd
-        call allocate_zvector(mg,rt%zj_e_tcd(itmp))
-      end do
-    end if
-    
+  
   else if(yn_out_micro_je=='y') then
   ! not single-scale but output microscopic current rt%j_e
     call allocate_vector(mg,rt%j_e)
+  end if
+
+  if(yn_transition_current_density=='y')then
+    if(yn_out_micro_je/='y' .and. .not.singlescale%flag_use)then
+      call allocate_vector(mg,rt%j_e)
+    end if
+    allocate(rt%zj_e_tcd(nmax_omega_tcd))
+    do itmp = 1, nmax_omega_tcd
+      call allocate_zvector(mg,rt%zj_e_tcd(itmp))
+    end do
   end if
 
   !-------------------------------------------------- Time evolution
